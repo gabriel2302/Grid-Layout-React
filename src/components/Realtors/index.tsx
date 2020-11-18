a6d388b752d4a0f7b2f95be5a89c20f62a09c049
gabriel2302 <gabriel23002@gmail.com>
2020-11-18 10:27:41 -0300

    Header Right

--- src/components/Realtors/index.tsx
+++ src/components/Realtors/index.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import Heading from '../Heading';
 
-import { Container } from './styles';
+import { Container, RealtorsList, RealtorDetails } from './styles';
+
+import realtor1 from '../../assets/img/realtor-1.jpeg';
+import realtor2 from '../../assets/img/realtor-2.jpeg';
+import realtor3 from '../../assets/img/realtor-3.jpeg';
 
 const Realtors: React.FC = () => {
   return (
     <Container>
-      Realtors
+      <Heading>
+        <h3>Top 3 Realtors</h3>
+      </Heading>
+      <RealtorsList>
+        <img src={realtor1} alt="Realtor 1"/>
+        <RealtorDetails>
+        <Heading light>
+          <h4>Erik Feinman</h4>
+        </Heading>
+        <p>245 houses sold</p>
+        </RealtorDetails>
+
+        <img src={realtor2} alt="Realtor 2"/>
+        <RealtorDetails>
+        <Heading light>
+          <h4>Kim Brown</h4>
+        </Heading>
+        <p>212 houses sold</p>
+        </RealtorDetails>
+
+        <img src={realtor3} alt="Realtor 3"/>
+        <RealtorDetails>
+        <Heading light>
+          <h4>Toby Ramsey</h4>
+        </Heading>
+        <p>198 houses sold</p>
+        </RealtorDetails>
+
+      </RealtorsList>
     </Container>
   );
 };
--- src/components/Realtors/styles.ts
+++ src/components/Realtors/styles.ts
@@ -4,4 +4,33 @@ import theme from '../../utils/theme';
 export const Container = styled.div`
   background-color: ${theme.colors.secondary};
   grid-column: col-start 7 / full-end;
+  padding: 3rem;
+
+  display: grid;
+  grid-row-gap: 2rem;
+  justify-content: center;
+  justify-items: center;
+  align-content: center;
+
+  img {
+    width: 7rem;
+    border-radius: 50%;
+    display: block;
+  }
+`;
+
+export const RealtorsList = styled.div`
+  display: grid;
+  grid-template-columns: min-content max-content;
+  grid-column-gap: 2rem;
+  grid-row-gap: 5vh;
+  align-items: center;
+`;
+
+export const RealtorDetails = styled.div`
+  p {
+    text-transform: uppercase;
+    color: ${theme.colors.greyLight2};
+    margin-top: -3px;
+  }
 `;
